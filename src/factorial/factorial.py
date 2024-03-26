cd53213e767aeee0ac282073b2968d19ef986ff0
santiago sigot <santisigot27mail.com>
2024-03-26 13:38:15 -0300

    factorial2

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,23 +6,25 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
-        print("Factorial de un número negativo no existe")
-
+        print("El factorial de un número negativo no existe")
+        return None
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
-        while(num > 1): 
+        while num > 1: 
             fact *= num 
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+if len(sys.argv) < 2:
+    num = int(input("Por favor, ingresa un número para calcular su factorial: "))
+else:
+    num = int(sys.argv[1])
+
+print("El factorial de", num, "! es", factorial(num))
+
 
